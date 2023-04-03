f12760bc1d66fef4690f71839ca38f037dbcca62
Your Name <you@example.com>
2023-04-03 19:18:51 -0300

    Cambiando cors

--- index.js
+++ index.js
@@ -12,22 +12,22 @@ app.use(express.json()); // Para que express lea los json
 
 conectarDB();
 
-const dominiosPertmitidos = [process.env.FRONTEND_URL]
+// const dominiosPertmitidos = [process.env.FRONTEND_URL]
 
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (dominiosPertmitidos.indexOf(origin) !== -1) {
-            //El origen del request esta permitido
-            callback(null, true)
-        } else {
-            callback(new Error('No permitido por CORS'))
-        }
-    }
-}
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//         if (dominiosPertmitidos.indexOf(origin) !== -1) {
+//             //El origen del request esta permitido
+//             callback(null, true)
+//         } else {
+//             callback(new Error('No permitido por CORS'))
+//         }
+//     }
+// }
 
 app.use(cors({
-    origin: '*'
-}));
+    origin: process.env.FRONTEND_URL
+   }));
 
 app.use('/api/veterinarios', veterinarioRoutes); // De esta forma accedo al router de veterinarios. app.tmb es una tura
 app.use('/api/pacientes', pacienteRoutes); // De esta forma accedo al router de veterinarios. app.tmb es una tura
